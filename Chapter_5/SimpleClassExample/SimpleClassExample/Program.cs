7809ba08bdb27e58acbf59dfaade9ad17c780efe
Mizda <mizdagbo@schaeffler.com>
2018-01-04 13:30:48 +0100

    reorder class uses according to the code in the book

--- Chapter_5/SimpleClassExample/SimpleClassExample/Program.cs
+++ Chapter_5/SimpleClassExample/SimpleClassExample/Program.cs
@@ -12,14 +12,6 @@ namespace SimpleClassExample
         {
             Console.WriteLine("***** Fun with Class Types *****\n");
 
-            // Make a Motorcycle with a rider named Tiny?
-            Motorcycle c = new Motorcycle(5);
-            c.SetDriverName("Tiny");
-            c.PopAWheely();
-            Console.WriteLine("Rider name is {0}", c.driverName); // Prints an empty name value!
-
-            Console.WriteLine();
-
             // Allocate and configure a Car object.
             Car myCar = new Car();
             myCar.petName = "Henry";
@@ -32,6 +24,15 @@ namespace SimpleClassExample
                 myCar.SpeedUp(5);
                 myCar.PrintState();
             }
+
+            Console.WriteLine();
+
+            // Make a Motorcycle with a rider named Tiny?
+            Motorcycle c = new Motorcycle(5);
+            c.SetDriverName("Tiny");
+            c.PopAWheely();
+            Console.WriteLine("Rider name is {0}", c.driverName); // Prints an empty name value!
+
             Console.ReadLine();
         }
 
